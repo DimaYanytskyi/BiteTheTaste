446db1b08ae98a725bed211c835b65d357c2fff4
DimaYanytskyi <yanytskyidima@gmail.com>
2023-09-26 22:51:54 +0300

    update

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ CHOOSE_SERVICE, ADDRESS, PHONE, COMPANY = range(4)
 def start(update: Update, context: CallbackContext) -> int:
     reply_keyboard = [['Order Services']]
     update.message.reply_text(
-        'Привіт!'
+        'Привіт!\n'
         'Я — бот команди Bite The Taste. З моєю допомогою зможеш всього за два кліка зробити замовлення на дегустацію!',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
     )
